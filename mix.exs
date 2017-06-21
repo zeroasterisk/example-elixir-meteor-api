18d7014010aea5e7359a8e755c113ff8246ea5d3
alan bount <alan@zeroasterisk.com>
2017-06-21 16:27:02 -0400

    add packages, resolve mongodb_ecto versions

--- mix.exs
+++ mix.exs
@@ -18,8 +18,14 @@ defmodule Meteorappapi.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Meteorappapi, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+     applications: [
+       :phoenix, :phoenix_pubsub, :phoenix_html,
+       :phoenix_ecto, :mongodb,
+       :cowboy, :logger, :gettext,
+       :poolboy, :uuid,
+       # :httpoison,
+       :absinthe_plug,
+    ]]
   end
 
   # Specifies which paths to compile per environment.
@@ -32,12 +38,21 @@ defmodule Meteorappapi.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.4"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
+     # the mongodb_ecto & phoenix_ecto, both need to require same {:ecto, "~> 2.0"}
+     {:phoenix_ecto, github: "phoenixframework/phoenix_ecto", ref: "v3.0.1"},
+     {:mongodb_ecto, github: "zeroasterisk/mongodb_ecto", branch: "elixir-ecto-v2.0.1"},
+     #{:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:timex, "~> 3.0"},
+     {:uuid, "~> 1.1" },
+     {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+     {:absinthe, "~> 1.3"},
+     # {:absinthe, github: "absinthe-graphql/absinthe", branch: "master"},
+     {:absinthe_plug, github: "absinthe-graphql/absinthe_plug", branch: "master"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
